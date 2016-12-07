9289729ab9e2805b88808c7cbae65ff8adb52eac
Anthony Parone <tony@parone.org>
2016-12-07 17:18:52 -0500

    commit

--- src/importfile.py
+++ src/importfile.py
@@ -2,6 +2,19 @@
 
 import re
 
+
+#INDI - 0 @P1@ INDI
+def parseINDI (linearray, countofarrayvalues):
+	if countofarrayvalues == 3: #expectedlength
+		previouslinepersonnumber = linepersonnumber
+		newlinepersonnumber = linearray[1]
+		print(linearray[1])
+		if newlinepersonnumber != previouslinepersonnumber:
+			linepersonnumber = newlinepersonnumber
+			print(linepersonnumber)
+	return;
+
+
 #from neo4j.v1 import GraphDatabase, basic_auth
 #driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "CIAshit2016h"))
 #session = driver.session()
@@ -15,19 +28,19 @@ import re
 
 
 gedcomfile = open("stonepark-tree.ged", "r")
-for line in gedcomfile:
-    #print line
-	if re.match("(.*)NAME(.*)", line):
-    	#print line
-		if line.find("/") <> -1:
-		####
-			firstslashposition = 0
-			#firstslashposition = line.find('/')
-			print "Found a Forward Slash"
-			print firstslashposition
-			print line
-		#else:
-    		#print "Found 'is' in the string."
-    		#########
-
+for gedcomline in gedcomfile:
+	#print(line)
+    #Parse the line by Spaces
+    linearray = gedcomline.split(" ")
+    print(linearray)
+    #Determine Length of array
+    countofarrayvalues = len(linearray)
+    if countofarrayvalues >= 3:
+    	#line context
+    	#print(linearray[2])
+        if linearray[0] == "1":
+        	linecontext = linearray[1]
+        if linearray[2] == "INDI": #new individual
+            parseINDI(linearray,countofarrayvalues)
+				
 #session.close()
