cf3ba388d78fea580d09563f12106e0d6e2df742
Anthony Parone <tony@parone.org>
2016-12-08 13:56:42 -0500

    added new functions

--- src/importfile.py
+++ src/importfile.py
@@ -3,44 +3,43 @@
 import re
 
 
+
+#Cleanup and parse the GEDCOM line
+def formatGEDCOMLINE (gedcomline):
+	gedcomline = gedcomline.replace("\n","")
+	linearray = gedcomline.split(" ")
+	return linearray;
+
 #INDI - 0 @P1@ INDI
-def parseINDI (linearray, countofarrayvalues):
+def parseINDI (linearray):
 	if countofarrayvalues == 3: #expectedlength
-		previouslinepersonnumber = linepersonnumber
-		newlinepersonnumber = linearray[1]
-		print(linearray[1])
-		if newlinepersonnumber != previouslinepersonnumber:
-			linepersonnumber = newlinepersonnumber
-			print(linepersonnumber)
+		linepersonnumber = linearray[1]
+		print(linepersonnumber)
+	return;
+	
+def parseNAME (linearray):
+	#Detemine how many names
+	namecount = len(linearray) - 2
+	for num, namepart in enumerate(linearray, start=2):
+		indexofforwardslashes = namepart.find("/") #Surname has slashes
+		if (indexofforwardslashes == 0): #this is a surname field
+			print(namepart)
 	return;
 
-
-#from neo4j.v1 import GraphDatabase, basic_auth
-#driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "CIAshit2016h"))
-#session = driver.session()
- #session.run("CREATE (a:Person {name:'Bob'})")
-		#result = session.run("MATCH (a:Person) RETURN a.name AS name")
-		#for record in result:
-   		#print(record["name"])
-		#if firstslashposition:
-        #	secondslashposition = line.index('/',firstslashposition+1)
-        #	print firstslashposition + ' ' + secondslashposition + ' ' + line
-
-
+#Begin reading GEDCOM file
 gedcomfile = open("stonepark-tree.ged", "r")
 for gedcomline in gedcomfile:
-	#print(line)
-    #Parse the line by Spaces
-    linearray = gedcomline.split(" ")
-    print(linearray)
-    #Determine Length of array
-    countofarrayvalues = len(linearray)
-    if countofarrayvalues >= 3:
-    	#line context
-    	#print(linearray[2])
-        if linearray[0] == "1":
-        	linecontext = linearray[1]
-        if linearray[2] == "INDI": #new individual
-            parseINDI(linearray,countofarrayvalues)
-				
-#session.close()
+	linearray = formatGEDCOMLINE(gedcomline)
+	countofarrayvalues = len(linearray)
+	if countofarrayvalues >= 3:
+		if (linearray[0] == "1"): linecontext = linearray[1]
+		if (linearray[2] == "INDI"): parseINDI(linearray)
+		if (linearray[1] == "NAME"): parseNAME(linearray)
+		#if (linearray[1] == "FAMS"):
+		#if (linearray[1] == "FAMC"):
+		#if (linearray[1] == "FAM"):
+		#if (linearray[1] == "HUSB"):
+		#if (linearray[1] == "WIFE"):
+		#if (linearray[1] == "CHIL"):
+		#if (linearray[1] == "SEX"):
+		#if (linearray[1] == "BIRT"):	
