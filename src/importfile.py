32e31d0fe4b1c8c6902c5defabe97275bb41d988
Anthony Parone <tony@parone.org>
2016-12-12 12:59:07 -0500

    Restructured included family

--- src/importfile.py
+++ src/importfile.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
-
+#need to look for suffix varioations
+#need to look for commas
+#is parsing the name really needed when we have person and family@ values
 import re
-
+linepersonnumber = ""
+linefamilynumber = ""
+linehusbpersonnumber = ""
+linewifepersonnumber = ""
+linechilpersonnumber = ""
 
 
 #Cleanup and parse the GEDCOM line
@@ -14,17 +20,61 @@ def formatGEDCOMLINE (gedcomline):
 def parseINDI (linearray):
 	if countofarrayvalues == 3: #expectedlength
 		linepersonnumber = linearray[1]
-		print(linepersonnumber)
+	return linepersonnumber;
+#NAME	
+def parseNAME (gedcomline, linepersonnumber, linearray):
+	namecount = len(linearray) - 2
+	surname = ""
+	nonsurname = ""
+	namesuffix = ""
+	#pull surname from between two slashes then ignore array values with slashes
+	positionfirstslash = 0
+	positionlastslash = 0
+	positionfirstslash = gedcomline.find("/",1)
+	if positionfirstslash > 0: positionlastslash = gedcomline.find("/", positionfirstslash+1)
+	if ((positionfirstslash > 0) & (positionlastslash > positionfirstslash)):
+		surname = gedcomline[positionfirstslash+1:positionlastslash]
+		
+	#nonsurname
+	positionfirstslash = 0
+	lengthoflinearray = len(linearray) - 1
+	for num, namepart in enumerate(linearray[2:], start=2):
+		#name suffix doesnt have a lsash and is last element and surname is not blank
+		if ((num == lengthoflinearray) & (namepart.find("/") == -1) & (surname <> "")): namesuffix = namepart
+		if ((num < lengthoflinearray) & (namepart.find("/") == -1)): nonsurname = nonsurname + namepart + " "
+	print linepersonnumber
+	print gedcomline
 	return;
 	
-def parseNAME (linearray):
-	#Detemine how many names
-	namecount = len(linearray) - 2
-	for num, namepart in enumerate(linearray, start=2):
-		indexofforwardslashes = namepart.find("/") #Surname has slashes
-		if (indexofforwardslashes == 0): #this is a surname field
-			print(namepart)
+#FAM
+#0 @F1@ FAM
+#1 HUSB @P1@
+#1 WIFE @P4@
+#1 CHIL @P5@
+def parseFAM (linearray):
+	if countofarrayvalues == 3: #expectedlength
+		linefamilynumber = linearray[1]
+		#print linefamilynumber
+	return linefamilynumber;
+
+def parseHUSB (linefamilynumber, linearray):
+	if countofarrayvalues == 3: #expectedlength
+		linehusbpersonnumber = linearray[2]
+	print linehusbpersonnumber + " is the husband in family " + linefamilynumber
 	return;
+
+def parseWIFE (linefamilynumber, linearray):
+	if countofarrayvalues == 3: #expectedlength
+		linewifepersonnumber = linearray[2]
+	print linewifepersonnumber + " is the wife in family " + linefamilynumber
+	return;
+	
+def parseCHIL (linefamilynumber, linearray):
+	if countofarrayvalues == 3: #expectedlength
+		linechilpersonnumber = linearray[2]
+	print linechilpersonnumber + " is the child in family " + linefamilynumber
+	return;
+
 
 #Begin reading GEDCOM file
 gedcomfile = open("stonepark-tree.ged", "r")
@@ -33,13 +83,13 @@ for gedcomline in gedcomfile:
 	countofarrayvalues = len(linearray)
 	if countofarrayvalues >= 3:
 		if (linearray[0] == "1"): linecontext = linearray[1]
-		if (linearray[2] == "INDI"): parseINDI(linearray)
-		if (linearray[1] == "NAME"): parseNAME(linearray)
+		if (linearray[2] == "INDI"): linepersonnumber = parseINDI(linearray)
+		if ((linearray[1] == "NAME") & (linearray[0] == "1")): parseNAME(gedcomline, linepersonnumber, linearray)
 		#if (linearray[1] == "FAMS"):
 		#if (linearray[1] == "FAMC"):
-		#if (linearray[1] == "FAM"):
-		#if (linearray[1] == "HUSB"):
-		#if (linearray[1] == "WIFE"):
-		#if (linearray[1] == "CHIL"):
+		if (linearray[2] == "FAM"): linefamilynumber = parseFAM(linearray)
+		if (linearray[1] == "HUSB"): parseHUSB(linefamilynumber, linearray)
+		if (linearray[1] == "WIFE"): parseWIFE(linefamilynumber, linearray)
+		if (linearray[1] == "CHIL"): parseCHIL(linefamilynumber, linearray)
 		#if (linearray[1] == "SEX"):
 		#if (linearray[1] == "BIRT"):	
